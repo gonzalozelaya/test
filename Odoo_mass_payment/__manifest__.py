80af10fc04ff628936a5e93f6d0944fd261207a5
Gonzalo Zelaya <gonzalo.zelaya@outsourcearg.com>
2024-07-22 21:45:13 -0300

    Changed manifest

--- Odoo_mass_payment/__manifest__.py
+++ Odoo_mass_payment/__manifest__.py
@@ -1,16 +1,17 @@
 {
     'name': 'Mass Payments',
     'version': '0.0.1',
-    'Author': 'AAAAAAAA',
-    'category': 'Accounting',
+    "author": "AAAAAAAA",
+    "license": "AGPL-3",
+    'installable': True,
+    'category': 'Payment',
     'summary': 'Manage Mass Payments',
     'description': """
         This module allows you to manage mass payments to multiple invoices.
     """,
-    'depends': ['account'],
+    'depends': ['account_payment'],
     'data': [
         'views/mass_payment_views.xml',
         'views/mass_payment_wizard_views.xml',
     ],
-    'installable': True,
 }
